hoshinolina/obs-pwvideo

commit 7f89fa21618555455254ce092fb27330e09b3e63

Fix compile failure due to pedantic asprintf error checking

src/plugin-main.c
@@ -434,8 +434,8 @@ static void populate_target_list(struct pipewire_video_capture *capture, obs_pro
 			}
 		} else {
 			char *label, *serial;
-			asprintf(&label, "%s (%d)", tgt->friendly_name, tgt->id);
-			asprintf(&serial, "#%" PRId64, tgt->serial);
+			assert(asprintf(&label, "%s (%d)", tgt->friendly_name, tgt->id) > 0);
+			assert(asprintf(&serial, "#%" PRId64, tgt->serial) > 0);
 			blog(LOG_INFO, "[pwvideo] Add string %s %s", label, serial);
 			prop_idx = obs_property_list_add_string(list, label, serial);
 

commit bcf8c70c63a953d45061ae3e943f4b22590fa5ca

Fix release build assert issue (oops)
Fixes: 7f89fa216185 ("Fix compile failure due to pedantic asprintf error checking")

src/plugin-main.c
@@ -434,8 +434,10 @@ static void populate_target_list(struct pipewire_video_capture *capture, obs_pro
 			}
 		} else {
 			char *label = NULL, *serial = NULL;
-			assert(asprintf(&label, "%s (%d)", tgt->friendly_name, tgt->id) > 0);
-			assert(asprintf(&serial, "#%" PRId64, tgt->serial) > 0);
+			int ret = asprintf(&label, "%s (%d)", tgt->friendly_name, tgt->id);
+			assert(ret > 0);
+			ret = asprintf(&serial, "#%" PRId64, tgt->serial);
+			assert(ret > 0);
 			blog(LOG_INFO, "[pwvideo] Add string %s %s", label, serial);
 			prop_idx = obs_property_list_add_string(list, label, serial);
 